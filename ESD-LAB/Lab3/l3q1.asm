1d0a7a9cfdad6ceea1c2650dcc083db80add7069
aaaaaanya <116056887+aaaaaanya@users.noreply.github.com>
2024-08-11 23:39:59 +0530

    Update l3q1.asm

--- ESD-LAB/Lab3/l3q1.asm
+++ ESD-LAB/Lab3/l3q1.asm
@@ -1,4 +1,4 @@
-;incomplete
+
 ;Write a program to add ten 32 bit numbers stored in code segment and store the result in data segment
 	AREA RESET,DATA,READONLY
 	EXPORT __Vectors
@@ -13,18 +13,28 @@ __Vectors
 	EXPORT Reset_Handler
 	
 Reset_Handler
+	LDR R0,=N
+	MOV R1,#10
+	LDR R5,=SUM
+	;LDR R3,=REM
+LOOP	
+	LDR R4,[R0],#4
+	ADDS R2,R4
+	ADC R3,#0
+	SUBS R1,#1
+	BNE LOOP
+	
+	STR R2,[R5],#4
+	STR R3,[R5]
+	
 
-MOV R0,#1 ;i
-LDR R1,=N
-LDR R2,=M
-CMP [R1],[R2]
 	
 STOP 
 	B STOP
 
-;N DCD 0x12345678,0x6A,0x234,0x49C,0xA13BC,0x69,0xA666,0x227799,0x98243231,0x01
-N DCD 12
-M DCD 30
+N DCD 1,2,3,4,5,6,7,8,9,10
+
 	AREA mydata,DATA,READWRITE
-LCM DCD 0
+SUM DCD 0,0
+;REM DCD 0
 	END
