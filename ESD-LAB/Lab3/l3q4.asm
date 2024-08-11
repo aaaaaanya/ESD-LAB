5049629c84d3486c060c659887743e9625f0cec1
aaaaaanya <116056887+aaaaaanya@users.noreply.github.com>
2024-08-11 23:40:23 +0530

    Update l3q4.asm

--- ESD-LAB/Lab3/l3q4.asm
+++ ESD-LAB/Lab3/l3q4.asm
@@ -1,5 +1,5 @@
-;incomplete
-;Write an assembly language program to find LCM of two numbers q
+;complete
+;Write an assembly language program to find LCM of two numbers 
 	AREA RESET,DATA,READONLY
 	EXPORT __Vectors
 	
@@ -13,13 +13,33 @@ __Vectors
 	EXPORT Reset_Handler
 	
 Reset_Handler
-	MOV R0,#1 ;i
 	LDR R1,=N
 	LDR R2,=M
-	;taking values
-	LDR R3,[R1]
-	LDR R4,[R2]
-	CMP R4,R3
+	LDR R1,[R1] ;12
+	LDR R2,[R2] ;30
+	MOV R0,#1 ;i
+	;finding lcm
+
+
+ 
+LOOP	MUL R3,R1,R0 
+	MOV R5,R3
+L2	CMP R5,R2 ;division to get remainder
+	BLO EXIT2
+	SUB R5,R2
+	B L2
+	;R3 now has remainder
+EXIT2	CMP R5,#0
+	BEQ EXIT
+	ADDNE R0,#1
+	B LOOP
+	
+
+	
+EXIT
+	MUL R3,R1,R0
+	LDR R4,=RESULT
+	STR R3,[R4]
 	
 STOP 
 	B STOP
@@ -28,5 +48,5 @@ STOP
 N DCD 12
 M DCD 30
 	AREA mydata,DATA,READWRITE
-SUM DCD 0
+RESULT DCD 0
 	END
