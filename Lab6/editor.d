b53713d66128194ee517dd0cd43a2f157ec0c57c
aaaaaanya <116056887+aaaaaanya@users.noreply.github.com>
2024-08-26 11:25:32 +0530

    Add files via upload

---
+++ Lab6/editor.d
@@ -0,0 +1,7 @@
+editor.o: editor.c
+editor.o: C:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
+editor.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+editor.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+editor.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+editor.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+editor.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
---
+++ Lab6/l6q1.c
@@ -0,0 +1,26 @@
+//blink LED
+
+#include <lpc17xx.h>
+
+
+//use Peripherals->GPIO fast interface->Port 0
+int main()
+{
+	int i;
+	SystemInit();
+	SystemCoreClockUpdate();
+	
+	LPC_PINCON->PINSEL0=0;
+	LPC_GPIO0->FIODIR=1<<4;
+	
+	//LPC_GPIO0->FIOSET=1<<4;
+	
+	while(1)
+	{
+		LPC_GPIO0->FIOSET=1<<4;
+		for(i=0;i<10000;i++);
+		LPC_GPIO0->FIOCLR=1<<4;
+		for(i=0;i<10000;i++);
+	}
+	
+}
