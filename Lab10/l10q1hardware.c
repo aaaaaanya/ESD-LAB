5dc787879340aea3f13f3c984ff84bdecb8335f0
Aanya Shantaram <116056887+aaaaaanya@users.noreply.github.com>
2024-10-30 15:40:53 +0530

    Update l10q1hardware.c

--- Lab10/l10q1hardware.c
+++ Lab10/l10q1hardware.c
@@ -1,7 +1,135 @@
-//not working, check
+//Write a c program to display the digital value representing the difference in analog voltages at ADC channel 4 and channel 5 on LCD.
+//hardware or burst mode
 
 //CONNECT cnd TO CNAD (lcd)
 
+//chatgpt code
+#include <LPC17xx.h>
+#include <stdio.h>
+
+int temp1, temp2, flag1, flag2;
+
+void port_write() {
+    int i;
+    LPC_GPIO0->FIOPIN = temp2 << 23;
+
+    if (flag1 == 0)
+        LPC_GPIO0->FIOCLR = 1 << 27;
+    else
+        LPC_GPIO0->FIOSET = 1 << 27;
+
+    LPC_GPIO0->FIOSET = 1 << 28;
+    for (i = 0; i < 50; i++);
+    LPC_GPIO0->FIOCLR = 1 << 28;
+
+    for (i = 0; i < 30000; i++);
+}
+
+void LCD_write() {
+    if ((flag1 == 0) & ((temp1 == 0x30) || (temp1 == 0x20)))
+        flag2 = 1;
+    else
+        flag2 = 0;
+
+    temp2 = temp1 >> 4;
+    port_write();
+
+    if (flag2 == 0) {
+        temp2 = temp1 & 0xF;
+        port_write();
+    }
+}
+
+void ADC_IRQHandler(void) {
+    float v1, v2, diff;
+    char vtg1[7], vtg2[7], diffStr[7];
+    int t1, t2,i;
+
+    // Check which ADC channel is ready
+    int status = LPC_ADC->ADSTAT & (3 << 4);
+    if (status == (1 << 4)) {
+        t1 = LPC_ADC->ADDR4 >> 4 & 0xFFF;  // Read ADC channel 4
+        v1 = (float)t1 * 3.3 / 0xFFF;  // Convert to voltage
+    } else if (status == (1 << 5)) {
+        t2 = LPC_ADC->ADDR5 >> 4 & 0xFFF;  // Read ADC channel 5
+        v2 = (float)t2 * 3.3 / 0xFFF;  // Convert to voltage
+    }
+
+    // Calculate the difference
+    diff = v1 - v2;
+
+    // Prepare strings for display
+    sprintf(vtg1, "%3.2fV", v1);
+    sprintf(vtg2, "%3.2fV", v2);
+    sprintf(diffStr, "%3.2fV", diff);
+
+    // Display V1
+    flag1 = 0;
+    temp1 = 0x84;  // Position for V1
+    LCD_write();
+    flag1 = 1;
+    for (i = 0; vtg1[i] != '\0'; i++) {
+        temp1 = vtg1[i];
+        LCD_write();
+    }
+
+    // Display V2
+    flag1 = 0;
+    temp1 = 0x8B;  // Position for V2
+    LCD_write();
+    flag1 = 1;
+    for (i = 0; vtg2[i] != '\0'; i++) {
+        temp1 = vtg2[i];
+        LCD_write();
+    }
+
+    // Display Difference
+    flag1 = 0;
+    temp1 = 0x92;  // Position for Difference
+    LCD_write();
+    flag1 = 1;
+    for (i = 0; diffStr[i] != '\0'; i++) {
+        temp1 = diffStr[i];
+        LCD_write();
+    }
+}
+
+int main() {
+	int i;
+	int lcd_init[] = {0x30, 0x30, 0x30, 0x20, 0x28, 0x0C, 0x01, 0x80, 0x06};
+    // Initialize system and peripherals
+    SystemInit();
+    SystemCoreClockUpdate();
+
+    LPC_PINCON->PINSEL1 = 0;  // GPIO
+    LPC_GPIO0->FIODIR = 0xF << 23 | 1 << 27 | 1 << 28;  // Set GPIO direction
+    flag1 = 0;
+
+    // LCD Initialization Sequence
+    
+    for (i = 0; i <= 8; i++) {
+        temp1 = lcd_init[i];
+        LCD_write();
+    }
+    flag1 = 1;
+
+    // Configure ADC
+    LPC_PINCON->PINSEL3 = (3 << 30) | (3 << 28);  // Set P1.31 as AD0.5 and P1.30 as AD0.4
+    LPC_SC->PCONP = (1 << 12);  // Enable ADC
+    LPC_SC->PCONP |= (1 << 15); // Power for GPIO block
+
+    // Enable ADC interrupts
+    LPC_ADC->ADCR = (1 << 4) | (1 << 5) | (1 << 21) | (1 << 16);  // Start ADC conversion
+    LPC_ADC->ADINTEN = (1 << 4) | (1 << 5);
+    NVIC_EnableIRQ(ADC_IRQn);
+
+    while (1);  // Loop indefinitely
+}
+
+//my modified code
+
+
+/*
 #include<LPC17xx.h>
 #include<stdio.h>
 int temp1, temp2, flag1, flag2;
@@ -43,9 +171,12 @@ int main()
 {
 unsigned int adc_temp;
 int i;
+float in_vtg;
 
-char msg1[]={" ANALOG IP:"};
-char msg2[]={"ADC OUTPUT:"};
+char msg1[]={"V1:"};
+char msg2[]={"D1:"};
+char msg3[]={"V2:"};
+char msg4[]={"D2:"};
 int lcd_init[] = {0x30, 0x30, 0x30, 0x20, 0x28, 0x0C, 0x01, 0x80, 0x06};
 SystemInit();
 SystemCoreClockUpdate();
@@ -60,12 +191,15 @@ temp1 = lcd_init[i];
 LCD_write();
 }
 flag1 = 1;
-LPC_PINCON->PINSEL3=3<<30|3<<28;//p1.31 as AD0.5
+LPC_PINCON->PINSEL3=3<<30;//p1.31 as AD0.5
 LPC_SC->PCONP=(1<<12);//enable peripheral adc
 LPC_SC->PCONP |= (1<<15); //Power for GPIO block
+
+//for first ad0.5
 flag1=0;
 temp1=0x80;
 LCD_write();
+
 flag1=1;
 i=0;
 while(msg1[i++]!='\0')
@@ -73,101 +207,134 @@ while(msg1[i++]!='\0')
 temp1=msg1[i];
 LCD_write();
 }
+
 flag1=0;
 temp1=0xC0;
 LCD_write();
 flag1=1;
 i=0;
+
 while(msg2[i++]!='\0')
 {
 temp1=msg2[i];
 LCD_write();
 }
-
-LPC_ADC->ADCR = (1<<5)|(1<<21)|(1<<24)|1<<4;//0x01200001; //ADC0.5, start conversion and operational
-LPC_ADC->ADINTEN=1<<4|1<<5;
-NVIC_EnableIRQ(ADC_IRQn);
-while(1);
-
-
-//for(i=0;i<2000;i++); //delay for conversion
-while(!( LPC_ADC->ADGDR >>31 == 0));//31st bit is done bit
-//wait till 'done' bit is 1, indicates conversion complete
-adc_temp = LPC_ADC->ADGDR;
-adc_temp >>= 4;
-adc_temp &= 0x00000FFF; //12 bit ADC
-in_vtg = ((float)adc_temp * 3.3/0xfff); //calculating input analog as 0.805mv is resolution
-//voltage
-sprintf(vtg,"%3.2fV",in_vtg);
-//convert the readings into string to display on LCD
-sprintf(dval,"%x",adc_temp);
+//for 2nd ad0.4
 flag1=0;
-temp1=0x8A;
+temp1=0x88;
+LCD_write();
+
+flag1=1;
+i=0;
+while(msg3[i++]!='\0')
+{
+temp1=msg3[i];
+LCD_write();
+}
+
+flag1=0;
+temp1=0xC8;
 LCD_write();
 flag1=1;
 i=0;
-while(vtg[i]!='\0')
+
+while(msg4[i++]!='\0')
 {
-temp1=vtg[i];i++;
+temp1=msg4[i];
 LCD_write();
-}
-flag1=0;
-temp1=0xCA;
-LCD_write();
-flag1=1;
-i=0;
-while(dval[i]!='\0')
-{
-temp1=dval[i];i++;
-LCD_write();
-}
-for(i=0;i<7;i++)
-vtg[i]=dval[i]=0;
 }
 
+while(1)
+{
+LPC_ADC->ADCR = 1<<4|(1<<5)|(1<<21)|(1<<16);
+	LPC_ADC->ADINTEN=1<<4|1<<5;
+	NVIC_EnableIRQ(ADC_IRQn);
+	while(1);
 
-void ADC_IRQHandler()
-{ int x;
-	float in_vtg;
-char vtg[7],dval[7];
-int channel, temp1, temp2;
-x = LPC_ADC->ADSTAT;
-x=x & 3<<4;
-if (x ==1<<4)
-{
-temp1 = LPC_ADC->ADDR4>>4;
-}
-else if (x ==1<<5)
-{ temp2 = LPC_ADC->ADDR5>>4;
-}
-in_vtg = ((float)temp1 * 3.3/0xfff); //calculating input analog as 0.805mv is resolution
-//voltage
-sprintf(vtg,"%3.2fV",in_vtg);
-//convert the readings into string to display on LCD
-sprintf(dval,"%x",temp1);
-flag1=0;
-temp1=0x8A;
-LCD_write();
-flag1=1;
-i=0;
-while(vtg[i]!='\0')
-{
-temp1=vtg[i];i++;
-LCD_write();
-}
-flag1=0;
-temp1=0xCA;
-LCD_write();
-flag1=1;
-i=0;
-while(dval[i]!='\0')
-{
-temp1=dval[i];i++;
-LCD_write();
-}
-for(i=0;i<7;i++)
-vtg[i]=dval[i]=0;
-
-//COMPLETE THE CODE TO DISPLAY THE DIFFERENCE ON MULTIPLEXED SEGMENTS
 }
 	
+}
+
+//
+void ADC_IRQHandler(void)
+{
+	float v1,v2;
+	char vtg1[4],dval1[4],vtg2[4],dval2[4];
+	int x,i;
+	int channel,t1,t2;
+	x=LPC_ADC->ADSTAT;
+	x=x&(3<<4);
+	if(x==1<<4)
+	{
+		t1=LPC_ADC->ADDR4>>4;
+		t1=t1&0xFFF;
+		v1 = ((float)t1 * 3.3/0xfff);
+	}
+	else if(x==1<<5)
+	{
+				t2=LPC_ADC->ADDR5>>4;
+				t2=t2&0xFFF;
+				v2 = ((float)t2 * 3.3/0xfff);
+	}
+	sprintf(vtg1,"%3.2fV",v1);
+	sprintf(vtg2,"%3.2fV",v2);
+	sprintf(dval1,"%x",t1);
+	sprintf(dval2,"%x",t2);
+	//for v1
+	flag1=0;
+temp1=0x84;
+LCD_write();
+flag1=1;
+i=0;
+
+while(vtg1[i]!='\0')
+{
+temp1=vtg1[i];i++;
+LCD_write();
+}
+//for v2
+
+	flag1=0;
+temp1=0x8B;
+LCD_write();
+flag1=1;
+i=0;
+
+while(vtg2[i]!='\0')
+{
+temp1=vtg2[i];i++;
+LCD_write();
+}
+
+//for d1
+
+	flag1=0;
+temp1=0xC4;
+LCD_write();
+flag1=1;
+i=0;
+
+while(dval1[i]!='\0')
+{
+temp1=dval1[i];i++;
+LCD_write();
+}
+
+//for d2
+
+	flag1=0;
+temp1=0xCB;
+LCD_write();
+flag1=1;
+i=0;
+
+while(dval2[i]!='\0')
+{
+temp1=dval2[i];i++;
+LCD_write();
+}
+for(i=0;i<4;i++)
+vtg1[i]=dval1[i]=dval2[i]=vtg2[i]=0;
+
+}
+*/
