929b40ce65388c640df89ec7b94ab5ef818c70e5
Aanya Shantaram <116056887+aaaaaanya@users.noreply.github.com>
2024-10-30 16:31:13 +0530

    Delete Rough C directory

--- Rough C/roughc.d
+++
@@ -1,8 +0,0 @@
-roughc.o: roughc.c
-roughc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
-roughc.o: C:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
-roughc.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
-roughc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-roughc.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-roughc.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-roughc.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
