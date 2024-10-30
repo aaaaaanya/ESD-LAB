2ec611c87de95b07d599b965d409bb6f3cb3a9d1
Aanya Shantaram <116056887+aaaaaanya@users.noreply.github.com>
2024-10-30 08:18:28 +0530

    Add files via upload

---
+++ Rough C/roughc.d
@@ -0,0 +1,8 @@
+roughc.o: roughc.c
+roughc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+roughc.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+roughc.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+roughc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+roughc.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+roughc.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+roughc.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
