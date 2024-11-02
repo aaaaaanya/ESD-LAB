cc76ebec721c0d68daa505246e4adeef7346ab0f
Aanya Shantaram <116056887+aaaaaanya@users.noreply.github.com>
2024-11-02 16:31:18 +0530

    Update ADC_PWM.c

--- Rough C/ADC_PWM.c
+++ Rough C/ADC_PWM.c
@@ -24,15 +24,15 @@ int main()
 	SystemInit();
 	SystemCoreClockUpdate();
 	LPC_SC->PCONP|=1<<12|1<<15;
-	LPC_PINCON->PINSEL3|=3<<;//FUNC3
+	LPC_PINCON->PINSEL3|=3<<30;//FUNC3
 	pwm_init();
 	while(1)	
 	{
-	  for(j = 0; j<10000; j++);
+	  //for(j = 0; j<10000; j++); //doesnt make a diff
 		LPC_ADC->ADCR=1<<5|1<<21|1<<24;
 		
 		while(!( LPC_ADC->ADGDR >>31 == 0));
-		adc_temp=(LPC_ADC->ADGDR>>4)&0XFF;
+		adc_temp=(LPC_ADC->ADGDR>>4)&0XFFF;
 		v=((float)adc_temp*3.3/0xfff);
 		v=v*100/3.3;
 		if (v > 100) v = 100; // Clamp to 100%
